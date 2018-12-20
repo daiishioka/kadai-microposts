9d695ccaf72e1ec7955853e304341b181997a1ae
dai.ishioka <dai.engr1107@gmail.com>
2018-12-20 08:19:23 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,9 @@ class ApplicationController < ActionController::Base
 		redirect_to login_url
 		end
 	end
+	
+	#Micropostの数をカウント
+	def counts(user)
+		@count_microposts = user.microposts.count
+	end
 end
